71a893c717f91cc03a64869d0677ddb55a7dc4db
mehranarbabian <mehranarbabian76@gmail.com>
2024-08-23 14:55:42 +0330

    init

--- find_tokens/find_divar_tokens.py
+++ find_tokens/find_divar_tokens.py
@@ -1,33 +1,36 @@
 import requests
 
-url = "https://api.divar.ir/v8/postlist/w/search"
 
-json  = {
-    "city_ids": ["1"],
-    "pagination_data": {
-        "@type": "type.googleapis.com/post_list.PaginationData",
-        "last_post_date": "2024-08-21T21:23:11.461693Z",
-        "page": 1,
-        "layer_page": 1
+def get_response_divar():
+    url = "https://api.divar.ir/v8/postlist/w/search"
 
-    },
-    "search_data": {
-        "form_data": {
-            "data": {
-                "category": {"str": {"value": "real-estate"}},
-                "sort": {"str": {"value": "sort_date"}}
+    json = {
+        "city_ids": ["1"],
+        "pagination_data": {
+            "@type": "type.googleapis.com/post_list.PaginationData",
+            "last_post_date": "2024-08-21T21:23:11.461693Z",
+            "page": 1,
+            "layer_page": 1
+
+        },
+        "search_data": {
+            "form_data": {
+                "data": {
+                    "category": {"str": {"value": "real-estate"}},
+                    "sort": {"str": {"value": "sort_date"}}
+                }
             }
         }
     }
-}
-headers ={
-    "Content-Type": "application/json"
-}
+    headers = {
+        "Content-Type": "application/json"
+    }
+    return requests.post(url, json=json, headers=headers)
 
-res = requests.post(url, json=json, headers=headers)
+
+res = get_response_divar()
 data = res.json()
 last_post_date = data["pagination"]["data"]["last_post_date"]
-
 
 list_of_tokens = []
 
@@ -35,23 +38,23 @@ count = 0
 while True:
 
     json = {
-    "city_ids": ["1"],
-    "pagination_data": {
-        "@type": "type.googleapis.com/post_list.PaginationData",
-        "last_post_date": last_post_date,
-        "page": 1,
-        "layer_page": 1
+        "city_ids": ["1"],
+        "pagination_data": {
+            "@type": "type.googleapis.com/post_list.PaginationData",
+            "last_post_date": last_post_date,
+            "page": 1,
+            "layer_page": 1
 
-    },
-    "search_data": {
-        "form_data": {
-            "data": {
-                "category": {"str": {"value": "real-estate"}},
-                "sort": {"str": {"value": "sort_date"}}
+        },
+        "search_data": {
+            "form_data": {
+                "data": {
+                    "category": {"str": {"value": "real-estate"}},
+                    "sort": {"str": {"value": "sort_date"}}
+                }
             }
         }
     }
-}
 
     res = requests.post(url, json=json, headers=headers)
     data = res.json()
@@ -66,6 +69,6 @@ while True:
     if count >= 100:
         break
 print(list_of_tokens)
-txt_file = open('tokens.txt','w',encoding='utf8')
+txt_file = open('tokens.txt', 'w', encoding='utf8')
 txt_file.write(','.join(list_of_tokens))
 txt_file.close()
