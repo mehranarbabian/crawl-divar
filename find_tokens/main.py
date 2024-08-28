f2c1bd08c33d774ecf7eaba3d24a8a4fd678764e
mehranarbabian <mehranarbabian76@gmail.com>
2024-08-29 02:08:50 +0330

    simple of use

--- find_tokens/main.py
+++ find_tokens/main.py
@@ -1,4 +1,31 @@
-from save_divar_record import save_divar_record
+from get_data_by_count_down_date import get_data
+from mongo_util import save_collection_mongo, clean_mongo
 from save_neighborhood_name import save_neighborhood
-save_divar_record()
-save_neighborhood()
+
+
+def main():
+    clean_mongo()
+    data_array = get_data()
+    items = []
+    for data in data_array:
+        for widget in data['list_widgets']:
+            token = widget["data"]["action"]["payload"]["token"]
+            title = widget["data"]["title"]
+            try:
+                # some time appartement has no price then has no middle_description_text field
+                price = widget["data"]["middle_description_text"]
+                price_without_unit = price.replace('تومان', '').strip()
+                token = widget["data"]["action"]["payload"]["token"]
+                # neighborhood_name = find_neighborhood_by_soup(token)
+                item = {"title": title, "price": price_without_unit, "token": token}
+
+                items.append(item)
+
+            except Exception as e:
+                continue
+    save_collection_mongo(items)
+    save_neighborhood()
+
+
+if __name__ == "__main__":
+    main()
